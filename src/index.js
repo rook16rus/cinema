3dc4cd8aa7cba41e6e09282dbc314c06829c69ad
rook16rus <kamalov00albert@gmail.com>
2021-05-12 10:37:23 +0300

    Connected the slider for the block with reviews

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import "normalize.css"
+import "../owl_carousel/dist/assets/owl.theme.default.min.css"
+import "../owl_carousel/dist/assets/owl.carousel.min.css"
+import "../owl_carousel/dist/owl.carousel.min"
 import "./scss/style.scss"
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    responsive: {
+        820: {
+            items: 3
+        },
+        560: {
+            items: 2
+        },
+        0: {
+            items: 1,
+            center: true
+        },
+    },
+    nav: true,
+})
